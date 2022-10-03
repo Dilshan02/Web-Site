041c2be4878aeafb8a4b207de91cc72887399116
tushan1 <tushanmeranga330@gmail.com>
2022-10-03 11:51:44 +0530

    Bug Fixed

--- app/src/main/java/com/example/safeme/Profile/Profile.java
+++ app/src/main/java/com/example/safeme/Profile/Profile.java
@@ -1,8 +1,13 @@
 package com.example.safeme.Profile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.safeme.R;
 
@@ -12,5 +17,35 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+
+    public Profile(int contentLayoutId) {
+        super(contentLayoutId);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        super.setContentView(view, params);
+    }
+
+    @Override
+    public boolean supportRequestWindowFeature(int featureId) {
+        return super.supportRequestWindowFeature(featureId);
+    }
+
+    @Override
+    public void onPanelClosed(int featureId, @NonNull Menu menu) {
+        super.onPanelClosed(featureId, menu);
     }
 }
