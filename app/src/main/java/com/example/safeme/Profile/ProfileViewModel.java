efdb923d337a8798e2c4b664ac136837f8e6b098
Therath01 <hmeranga454@gmail.com>
2022-10-11 18:31:23 +0530

    Profile view model Added

---
+++ app/src/main/java/com/example/safeme/Profile/ProfileViewModel.java
@@ -0,0 +1,29 @@
+package com.example.safeme.Profile;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+
+    public ProfileViewModel() {
+        super();
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
